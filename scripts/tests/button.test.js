4112535e00c51227bb654eb8669e31b3c53da839
michelleconville <michelle.conville@gmail.com>
2023-03-11 18:30:55 +0000

    Initial commit for the game

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -6,7 +6,7 @@ const buttonClick = require("../button");
 
 beforeAll(() => {
     let fs = require("fs");
-    let fileContents = fs.readFileSync("index.html", "utf-8");
+    let fileContents = fs.readFileSync("homepage.html", "utf-8");
     document.open();
     document.write(fileContents);
     document.close();
